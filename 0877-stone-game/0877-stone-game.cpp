33b8fd4ebf0b57831190d94db88117586c3b07f4
CharanRebba <159521050+Rebba-Charan@users.noreply.github.com>
2026-08-02 11:48:00 +0530

    Time: 11 ms (27.05%), Space: 11.5 MB (37.34%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+    int state[501][501];
+    int solve(int i,int j,vector<int>& piles)
+    {
+        if(i>j) return 0;
+        if(state[i][j]!=-1)
+        {
+            return state[i][j];
+        }
+        int pick_i = piles[i] + min(solve(i+2,j,piles),solve(i+1,j-1,piles));
+        int pick_j = piles[j] + min(solve(i+1,j-1,piles),solve(i,j-2,piles));
+        return (state[i][j] = max(pick_i,pick_j));
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int sum = accumulate(piles.begin(),piles.end(),0);
+        int n = piles.size();
+        int i = 0;
+        int j = n-1;
+        memset(state,-1,sizeof(state));
+        int alice_choice = solve(i,j,piles);
+        if(alice_choice > n/2) return true;
+        return false;
     }
 };
