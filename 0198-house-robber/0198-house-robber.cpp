32abeb7da1c9573eab8a8ec12977043323f5d01f
CharanRebba <159521050+Rebba-Charan@users.noreply.github.com>
2026-08-02 16:35:00 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,15 @@
 class Solution {
-    int dp[101];
-    int solve(int i,vector<int>& nums)
-    {
-        if(i>= nums.size()) return 0;
-        if(i == (nums.size() - 1)) return nums[i];
-        if(dp[i]!=-1) return dp[i];
-        int amount1 = nums[i] + solve(i + 2,nums);
-        int amount2 = nums[i+1] + solve(i + 3,nums);
-        return dp[i] = max(amount1,amount2);
-    }
 public:
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         int n = nums.size();
-        if(n == 2) return *max_element(nums.begin(),nums.end());
-        return solve(0,nums);
+        if(n <=2) return *max_element(begin(nums),end(nums));
+        vector<int> dp(n+1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2;i<n;i++)
+        {
+            dp[i] = max(nums[i] + dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
